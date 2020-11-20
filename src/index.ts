7430e711ace1a0f84ba417fb6c67453fbcbabdda
Jonathan <jonfleck88@gmail.com>
2020-11-20 18:14:56 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+type Arguments = {
+  importFunction: (controllerName: string) => Promise<any>;
+  onRegister: (controllerName: string, controller: any) => any;
+  attribute?: string;
+};
+
+export default function attributeImport({
+  importFunction,
+  onRegister,
+  attribute = "data-controller",
+}: Arguments) {
+  const registeredControllers = new Set();
+
+  const loadControllers = async (element: Element | Document) => {
+    element.querySelectorAll(`[${attribute}]`).forEach((c) => {
+      const controllerNames = c.getAttribute(attribute);
+
+      if (!controllerNames) return;
+
+      controllerNames.split(" ").forEach(async (controllerName) => {
+        if (!controllerName || registeredControllers.has(controllerName)) {
+          return;
+        }
+
+        registeredControllers.add(controllerName);
+
+        const { default: controller } = await importFunction(controllerName);
+
+        onRegister(controllerName, controller);
+      });
+    });
+  };
+
+  new MutationObserver(function callback(mutationList) {
+    mutationList.forEach(function ({ target }) {
+      if (target instanceof Element) {
+        loadControllers(target);
+      }
+    });
+  }).observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+
+  loadControllers(document);
+}
